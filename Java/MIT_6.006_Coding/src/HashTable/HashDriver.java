18f80f67f0180b85a440e420fafa8a9745290609
Sanjeet Tripathy <sanjeet.724@gmail.com>
2015-09-02 17:22:02 -0400

    Added some HashTable Code - insert and search

--- Java/MIT_6.006_Coding/src/HashTable/HashDriver.java
+++ Java/MIT_6.006_Coding/src/HashTable/HashDriver.java
@@ -10,12 +10,19 @@ public class HashDriver {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Reading the input file... ");
 		Scanner scanner = new Scanner(new File("inputKeys.txt"));
+		Table t = new Table();
 		while (scanner.hasNextInt()){
 			int  i = scanner.nextInt();
-			 System.out.println("Adding to the Table: " + i);
+			HashItem h = new HashItem(i);
+			t.putItem(h);
+			//System.out.println("Adding to the Table: " + i);
 		}
 		scanner.close();
-
+		Scanner scanner2 = new Scanner(new File("inputKeys.txt"));
+		System.out.println("Searching the table...");
+		while (scanner2.hasNextInt()){
+			// t.lookupItem(scanner2.nextInt());
+	    }
+		scanner2.close();
 	}
-
 }
--- Java/MIT_6.006_Coding/src/HashTable/Table.java
+++ Java/MIT_6.006_Coding/src/HashTable/Table.java
@@ -1,5 +1,45 @@
 package HashTable;
 
 public class Table {
-
+	int m = 20;
+	HashItem [] hashTable;
+	
+	public Table() {
+		this.hashTable = new HashItem[this.m];
+	}
+	
+	private int getHashKey(int n) {
+		return n % this.m;
+	}
+	
+	public void putItem(HashItem h) {
+		int index = this.getHashKey(h.key);
+		if ( this.hashTable[index] == null ){
+			System.out.println("Inserting at index: " + index);
+			this.hashTable[index] = h;
+		} 
+		else {
+			System.out.println("Collison during input");
+			// collisions
+		}
+	}
+	
+	public void lookupItem(int k) {
+		int index = this.getHashKey(k);
+		if (this.hashTable[index] != null) {
+			if (this.hashTable[index].key == k) {
+				System.out.println("Item Found: " + k + " at index: " + index);
+				return;
+			}
+			else {
+				System.out.println("Item was overwritten: " + k);
+				return;
+			}
+			//return this.hashTable[index];
+		}
+		System.out.println("Item Not Found: " + k);
+		return;
+		//return null;
+	}
+	
 }
