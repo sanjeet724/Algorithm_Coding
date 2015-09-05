865a648f2e65b720fc310d431833fafd9ba98991
Sanjeet Tripathy <sanjeet.724@gmail.com>
2015-09-05 09:56:05 -0400

    added log file for the hash table

--- Java/MIT_6.006_Coding/src/HashTable/HashDriver.java
+++ Java/MIT_6.006_Coding/src/HashTable/HashDriver.java
@@ -4,25 +4,20 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-
 public class HashDriver {
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Reading the input file... ");
 		Scanner scanner = new Scanner(new File("inputKeys.txt"));
 		Table t = new Table();
+		System.out.println("Adding items to the Table...");
 		while (scanner.hasNextInt()) {
 			int  i = scanner.nextInt();
 			HashItem h = new HashItem(i);
 			t.putItem(h);
-			// System.out.println("Adding to the Table: " + i);
 		}
 		scanner.close();
-		Scanner scanner2 = new Scanner(new File("inputKeys.txt"));
 		System.out.println("Searching the table...");
-		while (scanner2.hasNextInt()){
-			 t.lookupItem(scanner2.nextInt());
-	    }
-		scanner2.close();
+		t.writer.close();
 	}
 }
--- Java/MIT_6.006_Coding/src/HashTable/Table.java
+++ Java/MIT_6.006_Coding/src/HashTable/Table.java
@@ -1,10 +1,16 @@
 package HashTable;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
 public class Table {
-	int m = 17;
+	PrintWriter writer;
+	int m = 19;
 	HashItem [] hashTable;
 	
-	public Table() {
+	public Table() throws IOException{
+		this.writer = new PrintWriter("HashTable_Log.txt", "UTF-8");
 		this.hashTable = new HashItem[this.m];
 	}
 	
@@ -15,44 +21,43 @@ public class Table {
 	public void putItem(HashItem h) {
 		int index = this.getHashKey(h.key);
 		if ( this.hashTable[index] == null ){
-			System.out.println("Inserting at index: " + index);
+			writer.printf("Inserting key->%d at index: %d",h.key,index);
+			writer.println();
 			this.hashTable[index] = h;
 		} 
 		else {
-			System.out.println("Collison detected at index: " + index);
+			writer.println("Collison detected at index: " + index);
 			HashItem temp = this.hashTable[index];
 			while (temp.next != null) {
 				temp = temp.next;
 			}
 			temp.next = h;
+			writer.println("Key->" + h.key + " chained at index: " + index);
 		}
 	}
 	
-	public void lookupItem(int k) {
+	public HashItem lookupItem(int k) {
 		int index = this.getHashKey(k);
 		if (this.hashTable[index] != null) {
 			if (this.hashTable[index].key == k) {
-				System.out.println("Item Found: " + k + " at index: " + index);
-				return;
+				writer.println("Item Found: " + k + " at index: " + index);
+				return this.hashTable[index];
 			}
 			else {
-				System.out.println("Item was chained: " + k);
 				HashItem temp = this.hashTable[index];
 				while (temp != null) {
 					if (temp.key == k){
-						System.out.println("Item was found in the list");
-						return;
+						writer.println("Item was chained at index: " + index);
+						return this.hashTable[index];
 					}
 					temp = temp.next;
 				}
-				System.out.println("Item was not found in the list");
-				return;
+				System.out.println("Key not found in the chain: " + k);
+				return null;
 			}
-			//return this.hashTable[index];
 		}
-		System.out.println("Key Not Found: " + k);
-		return;
-		//return null;
+		System.out.println("Key Not Found in the Table: " + k);
+		return null;
 	}
 	
 }
