84e1e01feb171134115de8defae3145801c0cfd6
Sanjeet Tripathy <sanjeet.724@gmail.com>
2015-09-07 22:13:29 -0400

    added some table doubling code

--- Java/MIT_6.006_Coding/src/HashTable/Table.java
+++ Java/MIT_6.006_Coding/src/HashTable/Table.java
@@ -6,9 +6,9 @@ import java.io.PrintWriter;
 
 public class Table {
 	PrintWriter writer;
-	int m = 503; // table size
-	int n = 0;  // # of items in table
-	int occupied; // # of occupied slots
+	int m = 5;     // table size
+	int n = 0;      // # of items in table
+	int occupied;   // # of occupied slots
 	HashItem [] hashTable; 
 	
 	public Table() throws IOException{
@@ -18,15 +18,22 @@ public class Table {
 		this.n = 0;
 	}
 	
-	private int getHashKey(int n) {
-		return n % this.m;
+	
+	private int getHashIndex(int k) {
+		return k % this.m;
 	}
 	
 	public void putItem(HashItem h) {
+		// Table Doubling
+		if (this.n > this.m) {
+			writer.println("n is: " + this.n);
+			writer.println("m is: " + this.m);
+			this.tableDoubling();
+		}
 		this.n++;
-		int index = this.getHashKey(h.key);
+		int index = this.getHashIndex(h.key);
 		if ( this.hashTable[index] == null ){
-			writer.printf("Inserting key->%03d at index: %03d",h.key,index);
+			writer.printf("Inserting key->%03d at index: %02d",h.key,index);
 			writer.println();
 			this.hashTable[index] = h;
 			this.occupied++;
@@ -47,7 +54,7 @@ public class Table {
 	public HashItem lookupItem(int k) {
 		writer.println();
 		writer.println("Searching for key: " + k + "---->");
-		int index = this.getHashKey(k);
+		int index = this.getHashIndex(k);
 		if (this.hashTable[index] != null) {
 			if (this.hashTable[index].key == k) {
 				writer.println("Item Found: " + k + " at index: " + index);
@@ -70,18 +77,37 @@ public class Table {
 		return null;
 	}
 	
+	private void tableDoubling() {
+		writer.println("Doubling the Table and Re-Hasing...");
+		this.m = 2*this.m;
+		this.n = 0;
+		HashItem [] tempTable = this.hashTable;
+		this.hashTable = new HashItem[this.m];
+		for (int i = 0; i < tempTable.length; i++) {
+			if (tempTable[i] != null){
+				// iterate over the list at the slot
+				HashItem current = tempTable[i];
+				while (current != null){
+					this.putItem(current);
+					current = current.next;
+				}
+			}
+		}
+	}
+	
 	public void TableStats(){
 		writer.println();
 		writer.println("***Table Stats***");
 		writer.println("Table Size: " + this.m);
 		writer.println("Total # of items: " + this.n);
 		writer.println("Occupied Slots in Table: " + this.occupied);
-		writer.println("Free slots in the table(if any): ");
+		int freeSlots = 0;
 		for (int i = 0; i < this.m; i++){
 			if (this.hashTable[i] == null){
-				writer.println("Free Slot at index: " + i);
+				freeSlots++;
 			}
 		}
+		writer.println("# of free slots: " + freeSlots);
 	}
 	
 	
