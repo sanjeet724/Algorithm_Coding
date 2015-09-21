7f7ac18f886e8451083bfd7d95ee1af3f6eaef22
Sanjeet Tripathy <sanjeet.724@gmail.com>
2015-09-21 13:57:57 -0400

    A Test to understand how rolling hashes work

---
+++ Java/MIT_6.006_Coding/src/HashLab/Driver.java
@@ -0,0 +1,17 @@
+package HashLab;
+/* 
+This package is just for testing the various hash 
+functions and the Karp-Rabin algorithm 
+*/
+
+class Driver {
+
+	public static void main(String[] args) {
+		Tester test = new Tester();
+		test.getNumericValue("the");
+		test.getNumericValue("thei");
+		test.getNumericValue("hei");
+		
+	}
+
+}
---
+++ Java/MIT_6.006_Coding/src/HashLab/Tester.java
@@ -0,0 +1,29 @@
+package HashLab;
+
+public class Tester {
+	static long base = 100;
+	static long prime = 23;
+
+	
+	public Tester () {
+
+	}
+	
+	// returns an integer equivalent to the string
+	public long getNumericValue(String s){
+		System.out.println("Input String: " + s);
+		long sum = 0;
+		for (int i = 0 ;i < s.length(); i++){
+			sum = (sum + (long) s.charAt(i) * (long) Math.pow(base,i));
+		}
+		System.out.println("Numeric Value of String: " + sum);
+		this.HashIndex(sum);
+		return sum;
+	}
+	
+	private int HashIndex(long numericValue) {
+		long index = numericValue % prime;
+		System.out.println("Hash Index of s1: " + index);
+		return (int) index;
+	}
+}
